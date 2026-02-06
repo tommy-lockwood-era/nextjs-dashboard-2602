a2b27f2e0592f9c3c014a1418464c3050db4ee4f
Tommy Lockwood <t.lockwood@erasolutions.us>
2026-02-05 23:32:51 -0600

    Added a site-wide header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import Link from 'next/link';
 
 export default function RootLayout({
   children,
@@ -8,7 +9,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased px-6 pt-1`}>
+        <header className="flex justify-between items-center px-4 py-1 my-2 bg-gray-300 shadow-sm rounded-lg">
+          <Link href='/' className='font-bold'>Acme</Link>
+          <div className='flex gap-4'>
+            <Link href='/dashboard' className='hover:underline'>Dashboard</Link>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
