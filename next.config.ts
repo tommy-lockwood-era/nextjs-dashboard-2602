3308ece1f5d2162e82dbdfd770f016a17f9afe0f
Tommy Lockwood <t.lockwood@erasolutions.us>
2026-02-08 20:59:08 -0600

    Added authorized blob storage remote pattern

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ldqhy3xs1xoqq2fu.public.blob.vercel-storage.com',
+        // Optional: add port and pathname for more specific control
+      },
+    ],
+  },
 };
 
 export default nextConfig;
